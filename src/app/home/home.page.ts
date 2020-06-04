230b7f4f493038a84b1d4040027822706d5f4929
Karthikeyan36 <2017-18ece12@nec.edu.in>
2020-06-04 11:18:16 +0530

    moving myapp to github

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
-
+import { NavController } from '@ionic/angular';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-
-  constructor() {}
-
+export class HomePage { 
+  username:string;
+  password:string;
+  
+  constructor(private router:Router) {} 
+login(){
+  console.log("Username:"+ this.username);
+  console.log("Password:"+this.password)
+}  
+Register(){
+  this.router.navigate(['register']);
+}
 }
